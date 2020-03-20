706d02ec56d0afe12c9880e48a9924c131620c9d
acupxxx <205777870@qq.com>
2020-03-20 22:31:01 +0800

    edit by acupxxx

--- src/pers/garre/bean/Love.java
+++ src/pers/garre/bean/Love.java
@@ -4,7 +4,7 @@ public class Love {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Are you ok? editbyacupxxx");
+		System.out.println("edit by acupxxx");
 	}
 	
 }
