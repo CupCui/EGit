b518091eb1453fe0b307f774e626870173fd2d51
cupcui <garrettcui@outlook.com>
2020-03-21 09:04:28 +0800

    22222222222

--- src/pers/garre/bean/Love.java
+++ src/pers/garre/bean/Love.java
@@ -4,7 +4,7 @@ public class Love {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World123");
-		System.out.println("edit by acupxxx");
+		System.out.println("edit by acupxxx123");
 		System.out.println("edit by cupcui");
 	}
 	
