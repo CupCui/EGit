01cdc28836188c93c8814cd3c930dffabfffe104
cupcui <garrettcui@outlook.com>
2020-03-21 08:59:05 +0800

    edit first by cupcui

--- src/pers/garre/bean/Love.java
+++ src/pers/garre/bean/Love.java
@@ -3,7 +3,7 @@ package pers.garre.bean;
 public class Love {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World123");
 		System.out.println("edit by acupxxx");
 		System.out.println("edit by cupcui");
 	}
